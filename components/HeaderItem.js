27aa36f42b1883b31590478485e0f02a033f0bd8
Jaskaran Singh <262jaskaran@gmail.com>
2021-06-30 17:41:48 +0530

    Added project to github

---
+++ components/HeaderItem.js
@@ -0,0 +1,10 @@
+function HeaderItem({ title, Icon }) {
+    return (
+        <div className="flex items-center flex-col cursor-pointer group w-12 sm:w-20 hover:text-white">
+            <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+            <p className="opacity-0 tracking-widest group-hover:opacity-100">{title}</p>
+        </div>
+    )
+}
+
+export default HeaderItem
